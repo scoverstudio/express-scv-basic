8e581f4beabe879bd60cb127949919b625111f73
“Krystian” <scoverstudio@gmail.com>
2022-03-13 16:59:21 +0100

    add handlebars

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const path = require("path");
+const hbs = require("express-handlebars");
 
 const app = express();
+
+app.engine("hbs", hbs());
+app.set("view engine", "hbs");
 
 app.use(express.static(path.join(__dirname, "/public")));
 
@@ -13,20 +17,36 @@ app.use((req, res, next) => {
 });
 
 app.use("/user", (req, res, next) => {
-  res.show("forbidden.html");
+  res.render("forbidden");
 });
 
 app.get(["/", "/home"], (req, res) => {
-  res.show("index.html");
+  res.render("index");
 });
 
 app.get("/about", (req, res) => {
-  res.show("about.html");
+  res.render("about");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact", { layout: "dark" });
+});
+
+app.get("/info", (req, res) => {
+  res.render("info");
+});
+
+app.get("/history", (req, res) => {
+  res.render("history");
+});
+
+app.get("/hello/:name", (req, res) => {
+  res.render("hello", { name: req.params.name });
 });
 
 app.use((req, res) => {
   if (res.status(404)) {
-    res.show("404.html");
+    res.show("404");
   }
 });
 
